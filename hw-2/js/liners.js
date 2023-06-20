38fcaaea37c667c249dbd99cc7785cdd9b409ee5
v.komerystyi <v.komerystyi@gmail.com>
2023-06-20 11:40:18 +0300

    add task 10

--- hw-2/js/liners.js
+++ hw-2/js/liners.js
@@ -116,3 +116,16 @@ console.log("--- повних годин пройшло ")
 console.log(fullHours)
 
 //10. З клавіатури вводиться кількість монет номіналом 1 копійка. Визначити скільки гривень і копійок є у даній сумі . Наприклад, дано 245 копійок, а вивести 2 грн 45 копійок.
+
+const countMoney = 24505
+
+const resultGrnMoney = Math.floor(countMoney / 100)
+const resultCopMoney = countMoney % 100
+
+console.log(`${resultGrnMoney} грн ${resultCopMoney} копійок`)
+
+//11. Дано поточну кількість годи, хвилин, секунд (як на годиннику). Визначити 1) яка кількість секунд буде на годиннику через N секунд.2) яка кількість хвилин буде на годиннику через N секунд, X хвилин.
+
+const userTimeHour = 12
+const userTimeMinute = 12
+const userTimeSecond = 12
