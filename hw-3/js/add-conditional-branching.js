bd8dd31b837c60ea2c87424cc88a12a87ae3ea76
v.komerystyi <v.komerystyi@gmail.com>
2023-06-23 14:15:06 +0300

    optimized task 4

--- hw-3/js/add-conditional-branching.js
+++ hw-3/js/add-conditional-branching.js
@@ -74,24 +74,16 @@ const lengthField = parseInt(prompt("Кількість квадратів в п
 const positionShip = Math.floor(Math.random() * lengthField) + 1
 const userShot = parseInt(prompt(`Перший постріл від 1 до ${lengthField}`))
 
-if (userNumber !== positionShip) {
-    alert("промазав")
-    const userShot2 = parseInt(prompt(`Другий постріл від 1 до ${lengthField}`))
-    if (userShot2 === positionShip) {
-        alert("корабель потоплено")
-    } else {
-        alert("промазав")
-    }
-} else if (userShot === positionShip) {
-    alert("корабель потоплено")
-} else if (userShot - 1 <= positionShip && positionShip <= userShot + 1) {
+if (Math.abs(userShot - positionShip) <= 1) {
     alert("корабель пошкоджено")
     const userShot2 = parseInt(prompt(`Другий постріл від 1 до ${lengthField}`))
-    if (userShot2 === positionShip) {
-        alert("корабель потоплено")
-    } else {
-        alert("промазав")
-    }
+    userShot2 === positionShip ? alert("корабель потоплено") : alert("промазав")
+} else if (userShot === positionShip) {
+    alert("корабель потоплено")
+} else {
+    alert("промазав")
+    const userShot2 = parseInt(prompt(`Другий постріл від 1 до ${lengthField}`))
+    userShot2 === positionShip ? alert("корабель потоплено") : alert("промазав")
 }
 /* ===============================================================================
 Задача 5. 
