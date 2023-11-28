1dbf4727feaa776ff474ca054f43f13dff47f1fd
v.komerystyi <v.komerystyi@gmail.com>
2023-11-28 13:33:42 +0200

    task4

--- hw-18/js/script.js
+++ hw-18/js/script.js
@@ -37,7 +37,7 @@ function division(idSelector, sum, rate) {
 }
 currencyConverter()
  */
-function printUserAge(idUserDate, idUserAge) {
+/* function printUserAge(idUserDate, idUserAge) {
   let userYears = document.getElementById(idUserDate)
   userYears.oninput = function () {
     let userYearsValue = parseInt(userYears.value)
@@ -50,3 +50,29 @@ function countAge(userYears) {
   return currentYear - userYears
 }
 printUserAge('userDate', 'userAge')
+ */
+
+let wishesAll = ['love', 'money', 'peace', 'victory', 'luck']
+
+function randomIndex(arr) {
+  let randInx = Math.floor(Math.random() * arr.length)
+  let wish = arr[randInx]
+  return wish
+}
+
+window.onload = function () {
+  // Створили новий тег
+  let wishes1 = document.createElement('div')
+  let wishes2 = document.createElement('div')
+  let wishes3 = document.createElement('div')
+
+  // додаем нове
+  wishes1.innerHTML = `${randomIndex(wishesAll)}`
+  wishes2.innerHTML = `${randomIndex(wishesAll)}`
+  wishes3.innerHTML = `${randomIndex(wishesAll)}`
+  // знаходимо id тег куди хочемо додати
+  const app = document.getElementById('wish-app')
+  app.append(wishes1)
+  app.append(wishes2)
+  app.append(wishes3)
+}
